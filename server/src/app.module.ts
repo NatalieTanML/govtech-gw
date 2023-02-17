ba4ef202298d39956c1dd6158b06fa158305d17b
NatalieTanML <nat130613@gmail.com>
2023-02-17 16:43:53 +0800

    Fix dotenv

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UrlsModule } from './urls/urls.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
       `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     ),
